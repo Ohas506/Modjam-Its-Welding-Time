44559972b23df1cff0fb23e38b470b9c818d8330
Ohas506 <142551067+Ohas506@users.noreply.github.com>
2024-06-16 15:58:45 -0700

    -did a thing

---
+++ test.js
@@ -0,0 +1,24 @@
+if(e.question_number == 4 && Trump < 2 || Evengelical < 2 || Center < 2){campaignTrail_temp.questions_json[campaignTrail_temp.question_number + 1] = tunnel(12254);
+}  
+if(e.question_number == 4 && Trump < 2 || Evengelical < 2 || Center < 2){campaignTrail_temp.questions_json[campaignTrail_temp.question_number + 1] = tunnel(12254);
+} 
+
+&& Trump >= 2 && Evengelical >= 2 && Center >=2
+
+Trump >= 2 && Evengelical >= 2 && Center >=2
+
+if(e.question_number == 4 && Trump >= 2 && Evengelical >= 2 && Center >=2){campaignTrail_temp.questions_json[campaignTrail_temp.question_number + 1] = tunnel(381);
+    if(e.question_number == 4 && Trump < 2 || Evengelical < 2 || Center < 2){campaignTrail_temp.questions_json[campaignTrail_temp.question_number + 1] = tunnel(12254);
+    } 
+    if(e.question_number == 4 && Trump < 2 || Evengelical < 2 || Center < 2){campaignTrail_temp.questions_json[campaignTrail_temp.question_number + 1] = tunnel(12254);
+    },
+
+    if(campaignTrail_temp.question_number == 4 && Trump < 2 || Evengelical < 2 || Center < 2){campaignTrail_temp.questions_json[campaignTrail_temp.question_number + 1] = tunnel(12254);
+    }
+    && Evengelical >= 2 && Center >=2
+
+    
+    
+    
+
+    && Evengelical >= 2 && Center >= 2
